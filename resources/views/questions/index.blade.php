b81c11c47031214b2b1233064adf914a58896579
Emmanuel AGNIN <agnin_ehounou@yahoo.fr>
2020-01-10 10:59:23 +0000

    Add author info to the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -11,7 +11,11 @@
                   @foreach ($questions as $question)
                      <div class="media">
                          <div class="media-body">
-                            <h3>{{$question->title}}</h3>
+                            <h3><a href="{{ $question->url }}">{{$question->title}}</a></h3>
+                             <p class="lead">
+                                 <span>Ask by </span><a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                 <small class="text-muted">{{ $question->created_date}}</small>
+                             </p>
                                 {{ Str::limit($question->body,100) }}
                          </div>
                      </div>
