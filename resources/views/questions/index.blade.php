eca9aae42b886c452c4400cf2d8a4fb7e9d062bc
Emmanuel AGNIN <agnin_ehounou@yahoo.fr>
2020-01-10 22:21:23 +0000

    make a delete action

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -31,7 +31,14 @@
                          <div class="media-body ml-4">
                              <div class="d-flex justify-content-between">
                                 <h4><a href="{{ $question->url }}">{{$question->title}}</a></h4>
-                                 <a href="{{ route('questions.edit', $question) }}" class="btn btn-outline-info">Edit</a>
+                                <div>
+                                    <a href="{{ route('questions.edit', $question) }}" class="btn btn-outline-info">Edit</a>
+                                     <form class="form-delete" action="{{ route('questions.destroy', $question) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="submit" value="Delete" class="btn btn-outline-danger">
+                                    </form>
+                                </div>
                              </div>
 
                              <p class="lead">
