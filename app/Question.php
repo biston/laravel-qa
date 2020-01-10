f413b4668ba49c081ea8215e3fc34b735c178e38
Emmanuel AGNIN <agnin_ehounou@yahoo.fr>
2020-01-10 22:29:39 +0000

    Remake a delete action work again

--- app/Question.php
+++ app/Question.php
@@ -12,6 +12,10 @@ class Question extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 
     public function setTitleAttribute($value){
         $this->attributes['title']=$value;
